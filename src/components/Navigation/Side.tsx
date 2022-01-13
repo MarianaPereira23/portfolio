b319a7dd91cd70df09cf93c3eee8d605ed09473f
Mariana <mariana.c.pereira.23@hotmail.com>
2022-01-13 15:37:19 +0100

    Link to GitHub, LinkedIn and cv file

--- src/components/Navigation/Side.tsx
+++ src/components/Navigation/Side.tsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedinIn, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
-import { faFilePdf } from '@fortawesome/free-solid-svg-icons';
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
 import './Navigation.scss';
 
 const Side: React.FC = () => {
   return (
     <section className="page-left">
-      <a href="" className="left-links" target="_blank">
-        <FontAwesomeIcon icon={faFilePdf} className="links-icon"/>
+      <a href="../../files/MarianaPereira.pdf" className="left-links" download>
+        <FontAwesomeIcon icon={faFileDownload} className="links-icon"/>
       </a>
-      <a href="" className="left-links" target="_blank">
+      <a href="https://www.linkedin.com/in/mariana-pereira-5b3851156/" className="left-links" target="_blank" rel="noreferrer">
         <FontAwesomeIcon icon={faLinkedinIn} className="links-icon"/>
       </a>
-      <a href="" className="left-links" target="_blank">
+      <a href="https://github.com/MarianaPereira23" className="left-links" target="_blank" rel="noreferrer">
         <FontAwesomeIcon icon={faGithubSquare} className="links-icon"/>
       </a>
     </section>
