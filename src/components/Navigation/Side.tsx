d5d8689240c32792805e2c0c2a07e6c0541a3a6b
Mariana <mariana.c.pereira.23@hotmail.com>
2022-01-17 11:36:26 +0100

    Added skills list

--- src/components/Navigation/Side.tsx
+++ src/components/Navigation/Side.tsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLinkedinIn, faGithubSquare } from '@fortawesome/free-brands-svg-icons';
-import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
 import './Navigation.scss';
 
 const Side: React.FC = () => {
   return (
     <section className="page-left">
-      <a href="../../files/MarianaPereira.pdf" className="left-links" download>
-        <FontAwesomeIcon icon={faFileDownload} className="links-icon"/>
-      </a>
       <a href="https://www.linkedin.com/in/mariana-pereira-5b3851156/" className="left-links" target="_blank" rel="noreferrer">
         <FontAwesomeIcon icon={faLinkedinIn} className="links-icon"/>
       </a>
