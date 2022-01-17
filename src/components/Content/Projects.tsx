795879534e4eb79ac9f8e4de7c18accb7d25b2d9
Mariana <mariana.c.pereira.23@hotmail.com>
2022-01-17 12:44:55 +0100

    List for projects to add

--- src/components/Content/Projects.tsx
+++ src/components/Content/Projects.tsx
@@ -3,8 +3,14 @@ import './Content.scss';
 
 const Projects: React.FC = () => {
   return (
-    <div>
-      
+    <div className="page-projects">
+      <ul>
+        <li>GameHub</li>
+        <li>Real-time to do Typescript</li>
+        <li>Suggestionem</li>
+        <li>To do React</li>
+        <li>Libris</li>
+      </ul>
     </div>
   );
 };
