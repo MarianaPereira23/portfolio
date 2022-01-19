da797bf0a46fa0dcbf6f3cf29e3e9ed3cb93284f
Mariana <mariana.c.pereira.23@hotmail.com>
2022-01-19 10:27:24 +0100

    Added homepage

--- src/components/Content/Home.tsx
+++ src/components/Content/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Mariana from '../../media/images/Mariana.png';
 import './Content.scss';
 
 const Home: React.FC = () => {
   return (
-    <div>
-      
+    <div className="page-home">
+      <h1 className="home-title">Hi, <br/>I'm Mariana!</h1>
+      <img className="home-image" src={Mariana} alt="Mariana" />
     </div>
   );
 };
